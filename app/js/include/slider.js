a2fc12d16b23f640051bbaffc1715218f631d326
Khanh <Khanh13Nguyen13@gmail.com>
2020-05-09 19:09:14 +0300

    News section

--- app/js/include/slider.js
+++ app/js/include/slider.js
@@ -1,4 +1,4 @@
-$('.js-slickFeedback').slick({
+$('.js-initFeedback').slick({
   slidesToShow: 2,
   slidesToScroll: 2,
   infinite: true,
@@ -7,8 +7,15 @@ $('.js-slickFeedback').slick({
   appendArrows: $('.js-navFeedbk'),
   prevArrow: `<div class="prev-arrow"><img src="./img/icon-slick-nav.png" alt="icon-arrow" height="50" width="50"></div>`,
   nextArrow: `<div class="next-arrow"><img src="./img/icon-slick-nav.png" alt="icon-arrow" height="50" width="50"></div>`,
+});
 
-  // appendArrows: $('.feedback__arrow')
-
-  // icon-slick-nav
+$('.js-initNews').slick({
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  infinite: true,
+  dots: false,
+  arrows: true,
+  appendArrows: $('.js-navNews'),
+  prevArrow: `<div class="prev-arrow"><img src="./img/icon-slick-nav.png" alt="icon-arrow" height="50" width="50"></div>`,
+  nextArrow: `<div class="next-arrow"><img src="./img/icon-slick-nav.png" alt="icon-arrow" height="50" width="50"></div>`,
 });
