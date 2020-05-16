008a372160fa0b1081a6674fe08d693ebe0ca0d7
Khanh <Khanh13Nguyen13@gmail.com>
2020-05-17 01:55:31 +0300

    Personal page: tabs: partners program

--- app/js/include/tabs.js
+++ app/js/include/tabs.js
@@ -1,7 +1,15 @@
 $('.js-tabsLink').on('click', function(e) {
-  let id = $(this).attr('href')
+  let id = $(this).attr('href');
   $(this).closest('.tabs').siblings('.personal-inner').find('.holder-content').find('.tabcontent').removeClass('is-display')
   $(this).addClass('is-active').siblings('.js-tabsLink').removeClass('is-active')
   $('#' + id).addClass('is-display')
   e.preventDefault();
+})
+
+$('.js-programsLink').on('click', function(e) {
+  e.preventDefault();
+  let id = $(this).attr('href');
+  $(this).parent().siblings().find('.programs-content').removeClass('is-display');
+  $(this).addClass('is-active').siblings('.js-programsLink').removeClass('is-active');
+  $('#' + id).addClass('is-display')
 })
